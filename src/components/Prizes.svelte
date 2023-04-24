89eb5b12dd404db0cdf1ef061cd4bef0d558148e
Chandan <chandancvp08@gmail.com>
2023-04-24 18:31:56 +0530

    added prizes

--- src/components/Prizes.svelte
+++ src/components/Prizes.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { goto } from '$app/navigation';
 	import svgPrizes from '../assets/undraw_winners_re_wr1l.svg';
 </script>
 
@@ -22,8 +23,9 @@
 			<p class="mt-10 text-2xl tracking-wider">2nd Prize: <strong> 7,500 INR </strong></p>
 			<p class="mt-10 text-2xl tracking-wider">3rd Prize: <strong> 3,500 INR </strong></p> -->
 			<p class="mt-10 text-2xl tracking-wider">
-				<strong> PRIZES WILL BE ANNOUNCED SHORTLY </strong>
+				<strong> Prizes worth about 160K INR </strong>
 			</p>
+			<button class="p-6 rounded-md bg-purple-900 font-bold text-xl m-10" on:click={()=>goto("https://hack-solstice.devfolio.co/prizes" )}> Check out the prizes</button>
 		</div>
 	</div>
 	<p class="mt-20 text-white text-3xl font-bold font-mono tracking-wider">
